0005c9e00c6b6bea8cfc5fa11873870c487a80d5
Christian-Tirado-CODE <tiradoc02@gmail.com>
2021-01-24 18:28:31 -0400

    Bookmark feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,6 +4,8 @@ import recipeView from './views/recipeView';
 import searchView from './views/searchView';
 import resultsView from './views/resultsView';
 import paginationView from './views/paginationView';
+import bookMarksView from './views/bookmarksView';
+
 
 if (module.hot) {
   module.hot.accept();
@@ -19,6 +21,7 @@ const controlRecipes = async function () {
 
     // 0. Update results view to mark selected search result
       resultsView.update(model.getSearchResultsPage());
+      bookMarksView.update(model.state.bookmarks);
     // 1. Loading recipe
     await model.loadRecipe(id);
 
@@ -64,9 +67,25 @@ const controlServings = function(newServings) {
   recipeView.update(model.state.recipe);
 };
 
+const controlAddBookmark = function(){
+  // 1. Add/remove bookmark
+  
+  if(!model.state.recipe.bookmarked) 
+    model.addBookmark(model.state.recipe);
+  else 
+    model.deleteBookmark(model.state.recipe.id);
+  
+  // 2. Update recipe view
+    recipeView.update(model.state.recipe);
+  
+  // 3. Render bookmarks
+  bookMarksView.render(model.state.bookmarks);
+}
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   recipeView.addHandlerUpdateServings(controlServings);
+  recipeView.addHandlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView._addHandlerClick(controlPagination);
 };
